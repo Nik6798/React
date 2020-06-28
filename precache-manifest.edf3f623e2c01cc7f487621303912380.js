5e0ca926534119235c8f1e2e39b11a89b2cd9c75
nik6798 <nikhilsaini94169@gmail.com>
2020-06-28 08:23:52 +0530

    Updates

---
+++ precache-manifest.edf3f623e2c01cc7f487621303912380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0422108e74606760d0e565638d001fc5",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "1021b42c2e394986dd44",
+    "url": "/test/static/css/main.ce1c0962.chunk.css"
+  },
+  {
+    "revision": "d6f87e1f1d59baf8a77d",
+    "url": "/test/static/js/2.77a280a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.77a280a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1021b42c2e394986dd44",
+    "url": "/test/static/js/main.4a99963c.chunk.js"
+  },
+  {
+    "revision": "e7c2bc5ecae61921524f",
+    "url": "/test/static/js/runtime-main.959d0e93.js"
+  }
+]);
